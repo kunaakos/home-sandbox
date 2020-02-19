efde099d1b18a099e48beccb65be24b07c883c50
Kún Ákos <kunakos@kunakos-noti.local>
2020-02-19 10:49:01 +0100

    things should check if state mutations are possible and needed before attempting them

--- things/src/lib/thing.js
+++ things/src/lib/thing.js
@@ -2,6 +2,14 @@ function Thing(functions) {
 	Object.assign(this, functions)
 }
 
+const isWriteable = (mutators, key) => mutators.hasOwnProperty(key) && Boolean(mutators[key].set)
+const isReadable = (mutators, key) => mutators.hasOwnProperty(key) && Boolean(mutators[key].get)
+
+const checkPermissions = (mutators, key) => ({
+	isReadable: isReadable(mutators, key),
+	isWriteable: isWriteable(mutators, key)
+})
+
 export const makeThing = ({
 	type,
 	description,
@@ -9,13 +17,21 @@ export const makeThing = ({
 	mutators
 }) => {
 
-	// get is synchronous, so should all getters on things be
+	// get is synchronous
 	const get = () => {
 
 		const currentState = Object.entries(mutators)
+			.map(
+				([key, { get }]) => ([
+					key,
+					get
+						? get() // get current value of readable properties
+						: null // return null for write-only properties
+				])
+			) 
 			.reduce(
-				(acc, [key, { get }]) => {
-					acc[key] = get()
+				(acc, [key, value]) => {
+					acc[key] = value
 					return acc
 				},
 				{}
@@ -28,23 +44,49 @@ export const makeThing = ({
 		}
 	}
 
-	// set is an asynchronous operation, will complete when all values are updated
-	// it is the responsibility of thing implementations to ensure that setters are async and resolve after values were updated
+	// set will complete when all attempted mutations were finished
 	const set = async newState => {
-		const changes = await Promise.all(
-			Object.entries(newState)
-				.map(async ([key, newValue]) => {
-					if (mutators.hasOwnProperty(key)) {
-						return await mutators[key].set(newValue)
-							? key
-							: null
+
+		// an array of async functions that resolve with:
+		// ['key', null] - if a mutation wasn't attempted
+		// ['key', true] - if the mutation was successful
+		// ['key', false] - if the mutation failed
+		const mutations = Object.entries(newState)
+
+			// check if property exists and is writeable
+			.filter(
+				([key,]) => {
+					const { isReadable, isWriteable } = checkPermissions(mutators, key)
+					if (!isReadable && !isWriteable) {
+						console.warn(`Attempted mutation of inexistent property ${key} of ${description.id}.`)
+						return false
+					} else if (!isWriteable) {
+						console.warn(`Attempted mutation of read-only property ${key} of ${description.id}.`)
+						return false
 					} else {
-						return null // TODO: warn if non-existing key was changed
+						return true
 					}
-				})
-				.filter(Boolean) // filter out falsy values, only Promises should be left in the array
-		)
-		const changedKeys = changes.filter(Boolean)
+				}
+			)
+
+			// attempt a mutation if needed
+			.map(
+				async ([key, newValue]) => {
+					const attemptMutation = isReadable(mutators, key)
+						? mutators[key].get() !== newValue // no need for a mutation if the value hasn't changed
+						: true // write-only properties are always mutated
+
+					return attemptMutation
+						? [key, await mutators[key].set(newValue)]
+						: [key, null]
+				}	
+			)
+
+		const mutationResults = await Promise.all(mutations)
+		const changedKeys = mutationResults
+			.filter(([, result]) => Boolean(result)) // no need to publish changes for failed or unattempted mutations
+			.map(([key,]) => key)
+
 		if (changedKeys.length) {
 			publishChange(description.id)(changedKeys)
 		}
