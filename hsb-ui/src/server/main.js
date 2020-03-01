86e863204a7cf79a497a2d2780eb68abfcefd7b8
Kún Ákos <kunaakos@gmail.com>
2020-03-01 19:51:46 +0100

    set up http basic + jwt auth

--- hsb-ui/src/server/main.js
+++ hsb-ui/src/server/main.js
@@ -1,6 +1,11 @@
 const path = require('path')
 const express = require('express')
 const { createProxyMiddleware } = require('http-proxy-middleware')
+const cookieParser = require('cookie-parser')
+const passport = require('passport')
+const { BasicStrategy } = require('passport-http')
+const { Strategy: JwtStrategy } = require('passport-jwt')
+const jsonWebToken = require('jsonwebtoken')
 
 const { makeLogger } = require('hsb-service-utils/build/logger')
 
@@ -11,24 +16,108 @@ logger = makeLogger({
 	forceLogLevel: process.env.LOG_LEVEL
 })
 
+const USER_TOKEN_SECRET = process.env.USER_TOKEN_SECRET
+
 const main = async () => {
 
-	const app = express()
+	logger.debug('configuring authentication stategies')
 
-	app.use(
-		'/api/things',
-		createProxyMiddleware(
-			{
-				target: process.env.THINGS_URL,
-				ws: true,
-				logProvider: () => logger
+	passport.use(
+		new BasicStrategy(
+			(username, password, done) => {
+				if (
+					username === 'mario' &&
+					password === 'wario'
+				) {
+					done(null, { _id: 'its-a-me' })
+				} else {
+					done(null, false)
+				}
 			}
 		)
 	)
 
+	passport.use(
+		new JwtStrategy(
+			{
+				jwtFromRequest: req => (req.cookies && req.cookies['hsb_user_token']) || null,
+				secretOrKey: USER_TOKEN_SECRET,
+				issuer: 'remelem.online'
+			},
+			(userTokenPayload, done) => {
+				const { user } = userTokenPayload
+				done(null, user)
+			}
+		)
+	)
+
+	logger.debug('initializing middleware')
+
+	const parseCookies = cookieParser()
+	const serveSinglePageApp = express.static(path.join(__dirname, '../../build/client'))
+
+	const authenticateWithUserTokenCookie = passport.authenticate(
+		'jwt',
+		{
+			session: false,
+			failureRedirect: '/login'
+		}
+	)
+	const authenticateWithPassword = passport.authenticate(
+		'basic',
+		{
+			session: false
+		}
+	)
+
+	const proxyThingsApi = createProxyMiddleware(
+		{
+			target: process.env.THINGS_URL,
+			ws: true,
+			logProvider: () => logger
+		}
+	)
+
+	const setUserTokenCookie = (req, res) => {
+		res
+			.cookie(
+				'hsb_user_token',
+				jsonWebToken.sign(
+					{
+						user: req.user
+					},
+					USER_TOKEN_SECRET,
+					{
+						issuer: 'remelem.online',
+						expiresIn: '5m'
+					}
+				)
+			)
+			.redirect('/')
+	}
+
+	logger.debug('initializing app and registering routes')
+
+	const app = express()
+
+	app.use(
+		'/login',
+		authenticateWithPassword,
+		setUserTokenCookie
+	)
+
+	app.use(
+		'/api/things',
+		parseCookies,
+		authenticateWithUserTokenCookie,
+		proxyThingsApi
+	)
+
 	app.use(
 		'/',
-		express.static(path.join(__dirname, '../../build/client'))
+		parseCookies,
+		authenticateWithUserTokenCookie,
+		serveSinglePageApp
 	)
 
 	app.listen(process.env.UI_PORT, () => {
