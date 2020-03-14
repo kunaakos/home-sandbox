2a7286a193c3b2c427583f9462bb0cc1ff75cf37
Kún Ákos <kunaakos@gmail.com>
2020-03-14 15:05:38 +0100

    extract app component

--- hsb-ui/src/client/index.js
+++ hsb-ui/src/client/index.js
@@ -1,39 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import { ThemeProvider } from 'emotion-theming'
-import { lightTheme } from './themes/light-theme'
-
-import {
-	ThingsProvider,
-	useThings
-} from '../components/use-things'
-import { useUserToken } from '../components/use-user-token'
-
-import { ThingCard } from '../components/thing-cards'
-
-const App = () => {
-
-	useUserToken()
-	const { connected, things, setThing } = useThings()
-
-	const visibleThings = connected && things
-		? Object
-			.values(things)
-			.filter(thing => !thing.hidden)
-		: []
-
-	return (
-		<ThemeProvider theme={lightTheme}>
-			<h1>O HAI</h1>
-			<p>
-				I am currently {connected ? 'connected' : 'not connected'} {connected ? '😌' : '😞'}.<br />
-				{connected && <React.Fragment>I have a list of things that I can see in your home, let me know if you want me to do anything with them.</React.Fragment>}
-			</p>
-			{visibleThings.map(thing => <ThingCard key={thing.id} thing={thing} setThing={setThing} />)}
-		</ThemeProvider>
-	)
-}
+import { ThingsProvider } from '../components/use-things'
+import { App } from '../components/app'
 
 ReactDOM.render(
 	<ThingsProvider>
---
+++ hsb-ui/src/components/app.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+import { ThemeProvider } from 'emotion-theming'
+import { lightTheme } from '../themes/light-theme'
+
+import { useThings } from '../components/use-things'
+import { useUserToken } from '../components/use-user-token'
+
+import { ThingCard } from '../components/thing-cards'
+
+export const App = () => {
+
+	useUserToken()
+	const { connected, things, setThing } = useThings()
+
+	const visibleThings = connected && things
+		? Object
+			.values(things)
+			.filter(thing => !thing.hidden)
+		: []
+
+	return (
+		<ThemeProvider theme={lightTheme}>
+			<h1>O HAI</h1>
+			<p>
+				I am currently {connected ? 'connected' : 'not connected'} {connected ? '😌' : '😞'}.<br />
+				{connected && <React.Fragment>I have a list of things that I can see in your home, let me know if you want me to do anything with them.</React.Fragment>}
+			</p>
+			{visibleThings.map(thing => <ThingCard key={thing.id} thing={thing} setThing={setThing} />)}
+		</ThemeProvider>
+	)
+}
