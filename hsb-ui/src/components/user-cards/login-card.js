1f68245267ce3b288b9cf0bcb2e090978f98496c
Ákos Kun <kunaakos@gmail.com>
2021-02-12 23:58:33 +0100

    gardening: separate LoginCard

---
+++ hsb-ui/src/components/user-cards/login-card.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useRef } from 'react'
+
+export const LoginCard = ({ onSubmit }) => {
+
+	const usernameRef = useRef(null)
+	const passwordRef = useRef(null)
+
+	const submitHandler = e => {
+		e.preventDefault()
+		onSubmit && onSubmit({
+			username: usernameRef.current.value,
+			password: passwordRef.current.value
+		})
+	}
+
+	return (<div>
+		<form onSubmit={submitHandler}>
+
+			<label>username</label>
+			<br />
+			<input name="username" ref={usernameRef} />
+			<br />
+
+			<label>password</label>
+			<br />
+			<input type="password" name="password" ref={passwordRef} />
+			<br />
+
+			<input type="submit" />
+
+		</form>
+	</div>)
+}
--- hsb-ui/src/components/views/login-view.js
+++ hsb-ui/src/components/views/login-view.js
@@ -1,39 +1,7 @@
 import React from 'react'
-import { useRef } from 'react'
 
 import { CenteredCardContainer } from '../ui-kit/cards'
-
-const LoginCard = ({ onSubmit }) => {
-
-	const usernameRef = useRef(null)
-	const passwordRef = useRef(null)
-
-	const submitHandler = e => {
-		e.preventDefault()
-		onSubmit && onSubmit({
-			username: usernameRef.current.value,
-			password: passwordRef.current.value
-		})
-	}
-
-	return (<div>
-		<form onSubmit={submitHandler}>
-
-			<label>username</label>
-			<br />
-			<input name="username" ref={usernameRef} />
-			<br />
-
-			<label>password</label>
-			<br />
-			<input type="password" name="password" ref={passwordRef} />
-			<br />
-
-			<input type="submit" />
-
-		</form>
-	</div>)
-}
+import { LoginCard } from '../user-cards/login-card'
 
 export const LoginView = ({
 	login
